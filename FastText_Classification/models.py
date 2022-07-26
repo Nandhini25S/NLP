5e747ec9574859b596a56cb6d3c4d7f9997968f6
Sanjaypranav <sanjaypriya195@gmail.com>
2022-07-26 21:21:17 +0530

    implemented Bidirectional Lstm and solved some minor bugs

--- FastText_Classification/models.py
+++ FastText_Classification/models.py
@@ -1,22 +1,4 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM, Bidirectional
-from tensorflow.keras.callbacks import EarlyStopping
-import os
-os['TF_CPP_MIN_LOG_LEVEL'] = '3'
+"""
+To be implemented in the future:
+"""
 
-class LSTM:
-    def __init__(self, input_shape, output_shape, epochs, batch_size, verbose, early_stopping_path, patience):
-        self.input_shape = input_shape
-        self.output_shape = output_shape
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.verbose = verbose
-        self.early_stopping_path = early_stopping_path
-        self.patience = patience
-    
-    def build_model(self):
-        tf.compat.v1.disable_eager_execution()
-        model = Sequential()
-        model.add(Bidirectional(LSTM(128, return_sequences=True), input_shape=self.input_shape))
-        pass
