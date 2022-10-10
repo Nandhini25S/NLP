fdc64ebd219f4c4ded0cfdd931cc3148d2c97b1d
Sanjaypranav <sanjaypriya195@gmail.com>
2022-10-10 13:38:08 +0530

    edited convert.py

--- Name-Entity-Recognition/spacy-ner/convert.py
+++ Name-Entity-Recognition/spacy-ner/convert.py
@@ -1,9 +1,14 @@
 import json 
 import argparse
 import os
+from pprint import pprint
+from pydoc import describe
+from turtle import color
 import spacy
 from spacy.tokens import DocBin
 from tqdm import tqdm
+from rich import print as rprint
+from rich.progress import track
 
 # print(data)
 class ConvertToSpacy:
@@ -17,13 +22,14 @@ class ConvertToSpacy:
 
     def merge(self,dir_path):
         for i in os.listdir(dir_path):
-            with open(dir_path+"/"+i, 'r',encoding='utf8') as f:
+            rprint(f"[purple] {i}")
+            with open(dir_path+"/"+i, 'r',encoding='utf-8') as f:
                 data=json.load(f)
             self.train_data.append((data['annotations'][0][0],data['annotations'][0][1]))
         return 'file-merged succesfully'
     
     def Transform(self, file_path):
-        for text, annot in tqdm(self.train_data): # data in previous format
+        for text, annot in track(self.train_data,description="Transforming files..."): # data in previous format
             doc = self.nlp.make_doc(text) # create doc object from text
             ents = []
             for start, end, label in annot["entities"]: # add character indexes
@@ -46,6 +52,6 @@ class ConvertToSpacy:
 
 if __name__ == '__main__':
     cs = ConvertToSpacy()
-    cs.merge("/run/media/pranav/3CDEB4E6DEB4999A/Github/NLP/Name-Entity-Recognition/data/")
+    cs.merge("/media/pranav/3CDEB4E6DEB4999A/Github/NLP/Name-Entity-Recognition/data")
     # print(cs.get_data())
-    print(cs.Transform("/run/media/pranav/3CDEB4E6DEB4999A/Github/NLP/Name-Entity-Recognition/data/"))
+    print(cs.Transform("/media/pranav/3CDEB4E6DEB4999A/Github/NLP/Name-Entity-Recognition/spacy-data"))
