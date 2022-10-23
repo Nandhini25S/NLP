0cfa2cf048bb4bda084ff08f974b59bb3effad38
Sanjaypranav <sanjaypriya195@gmail.com>
2022-10-23 13:32:55 +0530

    added tokenizer

--- hf-classifier/main.py
+++ hf-classifier/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
 from transformers import BertTokenizer
+from vocably.preprocessing.text import Preprocessor as pp
 
 data = pd.read_csv('hf-classifier/data/movie_data.csv')
 # print(data.head())
@@ -15,9 +16,11 @@ target_encoder.fit(data['genre'])
 y = target_encoder.transform(data['genre'])
 
 
-#Now Tokenize the data using BERTT
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-encoding = tokenizer.encode_plus(data['overview'].values[0], add_special_tokens = True,  
-  truncation = True, padding = "max_length", return_attention_mask = True, return_tensors = "pt")
+#Now let's Preprocess the text
+preprocessor = pp(remove_links = True)
+data['overview'] = data['overview'].apply(preprocessor.normalize)
 
-print(len(encoding['input_ids'][0]))
+# Now let's tokenize the text
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',)
+tokenized = data['overview'].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
+print(tokenized[0])
